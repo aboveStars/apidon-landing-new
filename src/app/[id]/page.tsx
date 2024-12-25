b15a02e7cfaeab8e5b1130208cb0bacac82a0a07
aboveStars <yunuskorkmazis384@outlook.com>
2024-12-25 13:15:38 +0300

    Minor changes.

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -9,10 +9,6 @@ export default function Page() {
         <p className="mt-3 text-lg text-gray-300">
           You&apos;ll be redirected to the app in a few seconds.
         </p>
-        <p className="text-md mt-5 text-gray-400">
-          If you&apos;re using an Android device, the app will become available
-          soon. Stay tuned!
-        </p>
       </div>
     </div>
   );
